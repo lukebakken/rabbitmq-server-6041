e4f5da11c8d3d0358351aa2da5befc0f6eed7fb3
Luke Bakken <luke@bakken.io>
2022-10-07 14:16:12 -0700

    Generate files correctly

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 readonly dir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")/RabbitMQ Sérvér - Евгений"
-sed -e "s|@@PWD@@|$dir|" "$dir/rabbitmq.conf.in" > "$dir/rabbitmq.conf"
+sed -e "s|@@DIR@@|$dir|" "$dir/rabbitmq-env.conf.in" > "$dir/rabbitmq-env.conf"
+sed -e "s|@@DIR@@|$dir|" "$dir/rabbitmq.conf.in" > "$dir/rabbitmq.conf"
